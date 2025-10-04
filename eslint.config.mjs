777d8c86e60032119b09df148ab03a668dc9b1eb
lazarevic.miroslav <lazarevic.miroslav@gmail.com>
2025-10-04 21:32:21 +0000

    adding zard/ui

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,28 @@
 import eslint from '@eslint/js';
 import angular from 'angular-eslint';
+import unusedImports from 'eslint-plugin-unused-imports';
 import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
-import unusedImports from "eslint-plugin-unused-imports";
 
 export default defineConfig([
   {
-    ignores: ['jest.config.ts', 'setup-jest.ts'],
+    ignores: ['jest.config.ts', 'setup-jest.ts', 'src/app/shared/components/*'],
+  },
+  {
+    plugins: { 'unused-imports': unusedImports },
+    rules: {
+      'no-unused-vars': 'off', // or "@typescript-eslint/no-unused-vars": "off",
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+    },
   },
   {
     files: ['**/*.ts'],
--- src/app/start-page/start-page.ts
+++ src/app/start-page/start-page.ts
@@ -1,10 +1,18 @@
 import { Component, signal } from '@angular/core';
+import { ZardButtonComponent } from '@shared/components/button/button.component';
+import { ZardCardComponent } from '@shared/components/card/card.component';
 
 @Component({
   selector: 'app-start-page',
-  imports: [],
+  imports: [ZardButtonComponent, ZardCardComponent],
   templateUrl: './start-page.html',
 })
 export class StartPage {
   protected readonly title = signal('myapp');
+  protected readonly disabled = signal(false);
+
+  protected onClick() {
+    this.title.set('Zard UI');
+    this.disabled.set(true);
+  }
 }
